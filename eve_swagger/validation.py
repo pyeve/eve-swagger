374e3fa074ebde0eb18c1e1a27ecbe61a1dc5fb0
Nicola Iarocci <nicola@nicolaiarocci.com>
2015-10-04 21:50:46 +0200

    Validate that only allowed fields are included.

--- eve_swagger/validation.py
+++ eve_swagger/validation.py
@@ -9,19 +9,24 @@
 """
 from eve.exceptions import ConfigException
 from flask import current_app as app
+import urllib
 
 import eve_swagger
-
-# TODO since in swagger 'additional properties not allowed', we also
-# need to validate that there are not unknown fields in every (sub)document.
-# Or maybe have a relaxed approach to validation, and just ignore the issue
-# since swagger tooling will report the error anyway.
 
 
 def validate_info():
     _validate_required('info')
-
     info = app.config[eve_swagger.SWAGGER].get('info')
+
+    allowed = [
+        'title',
+        'description',
+        'termsOfService',
+        'contact',
+        'license',
+        'version'
+    ]
+    _validate_document(info, 'info', allowed)
 
     _validate_required('info.title')
     _validate_required('info.version')
@@ -30,17 +35,28 @@ def validate_info():
         contact = info.get('contact')
         if contact is None:
             return
-        _validate_document(contact, 'info.contact')
+
+        allowed = [
+            'name',
+            'url',
+            'email'
+        ]
+        _validate_document(contact, 'info.contact', allowed)
         _validate_url(contact.get('url'), 'info.contact.url')
+        _validate_email(contact.get('email'), 'info.contact.email')
 
     def validate_license():
         license = info.get('license')
         if license is None:
             return
-        _validate_document(license, 'info.license')
+
+        allowed = [
+            'name',
+            'url'
+        ]
+        _validate_document(license, 'info.license', allowed)
         _validate_required('info.license.name')
         _validate_url(license.get('url'), 'info.license.url')
-        _validate_email(license.get('email'), 'info.license.email')
 
     validate_contact()
     validate_license()
@@ -57,20 +73,28 @@ def _validate_required(key):
         node = node[part]
 
 
-def _validate_document(value, key):
+def _validate_document(value, key, fields):
     if not isinstance(value, dict):
         raise ConfigException('%s must be a dictionary' % key)
+    unknown = set(value.keys()) - set(fields)
+    if unknown:
+        raise ConfigException('fields allowed for "%s": %s' % (key, fields))
 
 
 def _validate_url(value, key):
-    # TODO
-    if value:
+    # TODO consider *not* validating this as both possible approaches (url
+    # opening/parsing or regex match) take a big performance hit
+    if value is None:
         pass
-    pass
+    try:
+        urllib.urlopen(value)
+    except IOError:
+        raise ConfigException('%s does not seem to be a valid (or reachable) '
+                              'url' % value)
 
 
 def _validate_email(value, key):
     # TODO
-    if value:
+    if value is None:
         pass
     pass
