532aa84158da69f0e95a314e2e59dcb9208c1a12
Nicola Iarocci <nicola@nicolaiarocci.com>
2016-06-11 10:45:54 +0200

    Bump version to 0.0.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ LONG_DESCRIPTION = open('README.rst').read()
 
 setup(
     name='Eve-Swagger',
-    version='0.0.3',
+    version='0.0.4',
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     author='Nicola Iarocci',
