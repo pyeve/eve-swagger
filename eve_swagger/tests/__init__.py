a7f2dbf03d88c0d8ef8a5b9c8947d903c3adac91
otibsa <otibsa@gmail.com>
2016-09-23 16:35:37 +0200

    First steps with test suite

---
+++ eve_swagger/tests/__init__.py
@@ -0,0 +1,58 @@
+import unittest
+import os
+import json
+
+import eve
+import eve_swagger
+from flask_pymongo import MongoClient
+from eve_swagger.tests.test_settings import MONGO_HOST, MONGO_PORT, \
+    MONGO_USERNAME, MONGO_PASSWORD, MONGO_DBNAME
+
+class TestBase(unittest.TestCase):
+    def setUp(self, settings=None):
+        self.this_directory = os.path.dirname(os.path.realpath(__file__))
+        if settings is None:
+            settings = os.path.join(self.this_directory, 'test_settings.py')
+
+        self.setupDB()
+
+        self.settings = settings
+        self.app = eve.Eve(settings=self.settings)
+
+        self.app.register_blueprint(eve_swagger.swagger)
+        self.app.config['SWAGGER_INFO'] = {
+            'title': 'Test eve-swagger',
+            'version': '0.0.1'
+        }
+
+        self.test_client = self.app.test_client()
+        self.domain = self.app.config['DOMAIN']
+
+        self.swagger_doc = self.get_swagger_doc()
+
+    def tearDown(self):
+        del self.app
+        self.dropDB()
+
+    def setupDB(self):
+        self.connection = MongoClient(MONGO_HOST, MONGO_PORT)
+        self.connection.drop_database(MONGO_DBNAME)
+        if MONGO_USERNAME:
+            self.connection[MONGO_DBNAME].add_user(MONGO_USERNAME,
+                                                   MONGO_PASSWORD)
+
+    def dropDB(self):
+        self.connection = MongoClient(MONGO_HOST, MONGO_PORT)
+        self.connection.drop_database(MONGO_DBNAME)
+        self.connection.close()
+
+    def get_swagger_doc(self):
+        r = self.test_client.get('/api-docs')
+        return self.parse_response(r)
+
+    def parse_response(self, r):
+        try:
+            v = json.loads(r.get_data())
+        except json.JSONDecodeError:
+            v = None
+        return v
---
+++ eve_swagger/tests/test_settings.py
@@ -0,0 +1,23 @@
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_USERNAME = 'test_user'
+MONGO_PASSWORD = 'test_pw'
+MONGO_DBNAME = 'eve_swagger_test'
+
+TRANSPARENT_SCHEMA_RULES = True
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+ITEM_METHODS= ['GET', 'PATCH', 'PUT', 'DELETE']
+
+DOMAIN = {
+    'people': {
+        'description': 'the people resource',
+        'schema': {
+            'name': {
+                'type': 'string',
+                'required': True,
+                'unique': True,
+                'description': 'the last name of the person'
+            },
+        }
+    }
+}
