a41bcf72c4c6a1bd60f6f5e99e92271fcd025da1
otibsa <otibsa@gmail.com>
2016-10-02 17:26:59 +0200

    Add more tests

--- eve_swagger/tests/test_settings.py
+++ eve_swagger/tests/test_settings.py
@@ -11,6 +11,7 @@ ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 DOMAIN = {
     'people': {
         'description': 'the people resource',
+        'type': 'dict',
         'schema': {
             'name': {
                 'type': 'string',
@@ -18,6 +19,46 @@ DOMAIN = {
                 'unique': True,
                 'description': 'the last name of the person'
             },
+            'job': {
+                'type': 'string',
+                'required': True,
+                'unique': True,
+                'description': 'the job of the person'
+            }
+        }
+    },
+    'disabled_resource': {
+        'disable_documentation': True,
+        'type': 'dict',
+        'schema': {
+            'field_1': {'type': 'string'}
+        }
+    },
+    'dr_resource_1': {
+        'type': 'dict',
+        'item_lookup_field': 'copied_field_with_description',
+        'schema': {
+            'copied_field_with_description': {
+                'type': 'string',
+                'description': 'foobar copied_field',
+                'data_relation': {
+                    'resource': 'people',
+                    'field': 'job'
+                }
+            },
+        }
+    },
+    'dr_resource_2': {
+        'type': 'dict',
+        'item_lookup_field': 'copied_field_without_description',
+        'schema': {
+            'copied_field_without_description': {
+                'type': 'string',
+                'data_relation': {
+                    'resource': 'people',
+                    'field': 'job'
+                }
+            }
         }
     }
 }
