5f0285e7a211514ac4c88f786647ed1c1a641739
Rui Lopes <rui.lourenco.lopes@gmail.com>
2018-08-09 15:46:47 +0100

    Add dict property in sub_resource and test the valueschema field.

--- eve_swagger/tests/tests.py
+++ eve_swagger/tests/tests.py
@@ -311,6 +311,20 @@ class TestEveSwagger(TestBase):
 
         self.assertIn(item_title, examples)
 
+    def test_dict_valueschema(self):
+        components = self.swagger_doc['components']
+        item_title = 'Sub_resource'
+        print(components['schemas'])
+        props = components['schemas'][item_title]['properties']
+        thedict = props['dictprop']
+        itemschema = self.domain['sub_resource']['schema']
+
+        self.assertEqual(thedict['type'], 'object')
+        self.assertIn('additionalProperties', thedict)
+        self.assertEqual(
+            thedict['additionalProperties']['type'],
+            itemschema['dictprop']['valueschema']['type'])
+
 
 if __name__ == '__main__':
     unittest.main()
