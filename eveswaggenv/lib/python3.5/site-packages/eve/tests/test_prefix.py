09f830fc21a55b1d3996bcbf6d07a4381407e667
Rui Lopes <rui.lourenco.lopes@gmail.com>
2019-05-06 18:52:05 +0100

    remove accidentaly added folder

--- eveswaggenv/lib/python3.5/site-packages/eve/tests/test_prefix.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-RESOURCE_METHODS = ['GET', 'POST']
-URL_PREFIX = 'prefix'
-DOMAIN = {'contacts': {}}
--- eveswaggenv/lib/python3.5/site-packages/eve/tests/test_prefix_version.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-URL_PREFIX = 'prefix'
-API_VERSION = 'v1'
-DOMAIN = {'contacts': {}}
