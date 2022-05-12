5f0cd683d41325c8c7ca1e4a799ab1cd4aa4794c
Roberto Romero <sildurin@gmail.com>
2022-05-12 11:19:40 +0200

    Adds security definitions for eve-auth-jwt
    Adds security definitions for eve-auth-jwt in security_schemes.
    Fixes pyeve/eve-swagger#121

--- eve_swagger/objects.py
+++ eve_swagger/objects.py
@@ -258,6 +258,15 @@ def security_schemes():
                 },
             }
         }
+    if "eve_auth_jwt" in sys.modules.keys():
+        return {
+            "BearerAuth": {
+                "type": "http",
+                "scheme": "bearer",
+                "bearerFormat": "JWT",
+                "description": "Bearer authentication scheme.",
+            }
+        }
     elif isinstance(app.auth, TokenAuth):
         return {"BearerAuth": {"type": "http", "scheme": "bearer"}}
     elif isinstance(app.auth, BasicAuth):
@@ -279,6 +288,8 @@ def callbacks():
 def security():
     if "flask_oauthlib.provider" in sys.modules.keys():
         return [{"oAuth2": []}]
+    elif "eve_auth_jwt" in sys.modules.keys():
+        return [{"BearerAuth": []}]
     elif isinstance(app.auth, TokenAuth):
         return [{"BearerAuth": []}]
     elif isinstance(app.auth, BasicAuth):
